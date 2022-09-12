091db69752e419141dbccb09df60e3d2dd344466
A-O-Emmanuel <emmanuel0058@gmail.com>
2022-09-12 14:26:47 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,16 +3,17 @@
 *
 * Return: Always 0 (success)
 */
+
 int main(void)
 {
-int c;
-c = 0;
-while
-(c < 10)
-{
-putchar(c + '0');
-c++;
-}
-putchar('\n');
-return (0);
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
 }
