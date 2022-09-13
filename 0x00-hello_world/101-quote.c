87f4497002921d5e1b18fd6f94a534c525854e92
A-O-Emmanuel <emmanuel0058@gmail.com>
2022-09-12 17:13:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write('2',"and the piece of art is useful\"-Dora Korpar,2015-10-19\n"'59');
+	write("and the piece of art is useful\" -Dora Korpar, 2015-10-19\n");
 
 	return(1);
 }
