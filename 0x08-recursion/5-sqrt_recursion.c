43576f6090714e0ae8dbb34bf9e19b39f0b72d3c
A-O-Emmanuel <emmanuel0058@gmail.com>
2022-10-01 15:11:04 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,7 +16,7 @@ int _sqrt_recursion(int n)
 /**
  * _sqrt - returns the square root of a number
  * @n: test number
- * @x: squred number
+ * @x: squared number
  *
  * Return: the square root of n
  */
