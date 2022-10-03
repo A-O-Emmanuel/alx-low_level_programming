c1729381a347224a0cebefc85b6e024c1785118a
A-O-Emmanuel <emmanuel0058@gmail.com>
2022-10-03 13:30:27 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,32 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
 
 /**
- * main - Prgram that adds positive numbers
- * @argc: This is the argument count
- * @argv: This is the argument vector
+ * main - adds nubmers
+ * @argc: number of arguments passed to the function
+ * @argv: arguments vector of pinters to srings
  *
- * Return: 0;
+ * Return: 0 if no errors, else 1
  */
+
 int main(int argc, char *argv[])
 {
-	int sum = 0, i;
+	int a = 0, i, j;
 
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			int b;
-			char *str;
-
-			str = argv[i];
-			for (b = 0; str[b] != '\0'; b++)
+			if (isdigit(argv[i][j]) == 0)
 			{
-				if (str[b] < 48 || str[b] > 37)
-				{
-					printf("Error\n");
-					return (1);
-				}
+				puts("Error");
+				return (1);
 			}
 		}
 	}
 	for (i = 1; i < argc; i++)
 	{
-		sum += atoi(argv[i]);
+		a += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
-	return (0);
-}
+	printf("%d\n", a);
