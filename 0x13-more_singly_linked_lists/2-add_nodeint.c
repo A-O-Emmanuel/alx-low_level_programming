63ab12305fdf7f60630d4e36f6aa38362611e097
A-O-Emmanuel <emmanuel0058@gmail.com>
2022-10-31 13:51:12 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 #include <stdlib.h>
-
 /**
  * add_nodeint - adds a node to the biginning of a linked list
- * @head; pointer to the head of the list
+ * @head: pointer to the head of the list
  * @n: integer to be used as content
  *
  * Return: address of the newly added node
